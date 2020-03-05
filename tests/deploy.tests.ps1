0ddc193c45273c5b9ff650418ae64955d563ef17
gallowst <31688948+gallowst@users.noreply.github.com>
2020-03-05 17:36:17 +0000

    changed the test

--- tests/deploy.tests.ps1
+++ tests/deploy.tests.ps1
@@ -1,17 +1,13 @@
 Describe  'ACI Demo Check' {
     Context 'Checking ACI demo Container' {
-        # Assume the test will fail
-        $bolTestpass = $false
-        # Test once every 60 seconds
+        $strUri = ("http://aci-demo-{0}.{1}.azurecontainer.io" -f $(Get-Date -f yyyyMMdd), $env:location)
         foreach ($i in 1..60) {
-            $resp = Invoke-WebRequest -Uri ("http://aci-demo-{0}.{1}.azurecontainer.io" -f $(Get-Date -f yyyyMMdd), $env:location)
-            if ($resp.StatusCode -eq 200) {$bolTestPass = $true}
-            else {$bolTestpass = $false}
-            start-sleep -Seconds 1
-            # Perform the test
+            $resp = Invoke-WebRequest -Uri $strUri
             It 'Container should respond on port 80' {
-                $bolTestPass | Should -Be $true
+                $resp.StatusCode | Should -Be 200
             }
+            Clear-Variable resp
+            start-sleep -Seconds 1
         }       
     }
 }
