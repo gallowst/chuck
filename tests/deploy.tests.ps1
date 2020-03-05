7d8cf4600150879655ebe83a0231946c596438bd
gallowst <31688948+gallowst@users.noreply.github.com>
2020-03-05 17:06:19 +0000

    Moved tests

---
+++ tests/deploy.tests.ps1
@@ -0,0 +1,17 @@
+Describe  'ACI Demo Check' {
+    Context 'Checking ACI demo Container' {
+        # Assume the test will fail
+        $bolTestpass = $false
+        # Test once every 60 seconds
+        foreach ($i in 1..60) {
+            $resp = Invoke-WebRequest -Uri ("http://aci-demo-{0}.{1}.azurecontainer.io" -f $(Get-Date -f yyyyMMdd), $env:location)
+            if ($resp.StatusCode -eq 200) {$bolTestPass = $true}
+            else {$bolTestpass = $false}
+            start-sleep -Seconds 1
+        }
+        # Report the test status
+        It 'Container should respond on port 80' {
+           $bolTestPass | Should -Be $true
+        }
+    }
+}
