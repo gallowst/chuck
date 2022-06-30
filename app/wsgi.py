e465fb509027298d7166f98acf60292d5f32cf2d
gallowst <31688948+gallowst@users.noreply.github.com>
2022-06-30 14:11:36 +0000

    Added pylint

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, Response
 from html import unescape
 import json
-import requests
 import socket
+import requests
+from flask import Flask, render_template #, Response
 
 application = Flask(__name__,)
 
 @application.route("/")
 def index():
-   """Chuck home page."""
-   # Retrieve JSON document
-   full = json.loads((requests.get("http://api.icndb.com/jokes/random?exclude=[explicit]")).text)
-   # Clean up the HTML joke
-   unescaped_joke = unescape(full['value']['joke'])
-   return render_template('chuck.html',joke = unescaped_joke, container=socket.gethostname())
+    """Chuck home page."""
+    # Retrieve JSON document
+    full = json.loads((requests.get("http://api.icndb.com/jokes/random?exclude=[explicit]")).text)
+    # Clean up the HTML joke
+    unescaped_joke = unescape(full['value']['joke'])
+    return render_template('chuck.html',joke = unescaped_joke, container=socket.gethostname())
 
 if __name__ == "__main__":
     application.run()
