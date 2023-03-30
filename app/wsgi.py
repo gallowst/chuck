a0958414f2a20bfc37fb7cfaba48d7fe1c83e058
Stuart Galloway <stuart.galloway@auxilion.com>
2023-03-30 13:58:47 +0100

    Chat

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,19 +1,18 @@
 from html import unescape
 import json
-import socket
-import requests
 from flask import Flask, render_template
+from requests import get
 
-chuck = Flask(__name__,)
+app = Flask(__name__)
 
-@chuck.route("/")
+@app.route("/")
 def index():
     """Chuck home page."""
     # Retrieve JSON document
     full = json.loads(get("https://api.chucknorris.io/jokes/random?category=dev").content)
     # Clean up the HTML joke
     unescaped_joke = unescape(full['value'])
-    return render_template('chuck.html', joke=unescaped_joke, container=socket.gethostname())
+    return render_template('chuck.html', joke=unescaped_joke)
 
 if __name__ == "__main__":
-    chuck.run()
+    app.run(debug=False)
