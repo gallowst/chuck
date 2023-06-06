d0f86b5efd1b1cddc750a9e782e97cd3428e16fc
gallowst <31688948+gallowst@users.noreply.github.com>
2023-06-06 09:57:30 +0100

    Update wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -12,7 +12,7 @@ def index():
     full = json.loads(get("https://api.chucknorris.io/jokes/random?category=dev").content)
     # Clean up the HTML joke
     unescaped_joke = unescape(full['value'])
-    return render_template('chuck.html', joke=unescaped_joke)
+    return render_template('chuck.html', joke=unescaped_joke, container=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(debug=False)
